f0b8c3a16c2703c1fafc763b0199b8b4750b5054
Bharatvaj H <bharatvajph@gmail.com>
2018-05-23 21:21:17 +0530

    Added cards and page resources, added dimnens resource, buttons in header navigate to the corresponding activities

--- app/src/main/java/com/example/laz3r/emergencymedicalapp/MainActivity.java
+++ app/src/main/java/com/example/laz3r/emergencymedicalapp/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.laz3r.emergencymedicalapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Context context = this;
+
+    ImageView headerUserImage;
+    TextView headerUserName;
+    TextView headerUserHealth;
+
+    Button headerHelplineButton;
+    Button headerHeartButton;
+    Button headerAlarmButton;
+    Button headerNewsButton;
+
+    void initializeLayout(){
+        headerUserImage = findViewById(R.id.headerUserImage);
+        headerUserName = findViewById(R.id.headerUserName);
+        headerUserHealth = findViewById(R.id.headerUserHealth);
+
+        headerHelplineButton = findViewById(R.id.headerHelplineButton);
+        headerHeartButton = findViewById(R.id.headerHeartButton);
+        headerAlarmButton = findViewById(R.id.headerAlarmButton);
+        headerNewsButton = findViewById(R.id.headerNewsButton);
+
+    }
+    void assignListeners(){
+        headerHelplineButton.setOnClickListener(this);
+        headerHeartButton.setOnClickListener(this);
+        headerAlarmButton.setOnClickListener(this);
+        headerNewsButton.setOnClickListener(this);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeLayout();
+        assignListeners();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = null;
+        switch (v.getId()){
+            case R.id.headerHelplineButton:
+                i = new Intent(this, HelplineActivity.class);
+                break;
+            case R.id.headerHeartButton:
+                i = new Intent(this, HeartMonitorActivity.class);
+                break;
+            case R.id.headerAlarmButton:
+                i = new Intent(this, AlarmActivity.class);
+                break;
+            case R.id.headerNewsButton:
+                i = new Intent(this, NewsActivity.class);
+                break;
+        }
+        context.startActivity(i);
     }
 }
